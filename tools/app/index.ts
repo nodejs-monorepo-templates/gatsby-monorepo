2744e842ecc6c4d370757e0714a851c25ee24f73
khai96_ <hvksmr1996@gmail.com>
2020-08-09 11:37:54 +0700

    Create tools/app

---
+++ tools/app/index.ts
@@ -0,0 +1,33 @@
+import process from 'process'
+import { spawnSync } from 'child_process'
+import { dbg } from 'string-template-format-inspect'
+import { app } from '@tools/places'
+
+const {
+  NODE_SCRIPT_RUNNER = 'pnpm run',
+} = process.env
+
+const [command, ...prefix] = NODE_SCRIPT_RUNNER.split(/\s+/)
+
+if (!command) {
+  throw new TypeError(dbg`Invalid NODE_SCRIPT_RUNNER: ${NODE_SCRIPT_RUNNER}`)
+}
+
+const spawnOptions = {
+  stdio: 'inherit',
+  cwd: app,
+} as const
+
+export function runScript(suffix: readonly string[]) {
+  const { error, status } = spawnSync(command, [...prefix, ...suffix], spawnOptions)
+  if (error) throw error
+  return {
+    exit() {
+      if (!status) return
+      console.error(
+        dbg`Command '${[command, ...prefix, ...suffix]}' exits with non-zero status: ${status}`,
+      )
+      throw process.exit(status)
+    },
+  }
+}
