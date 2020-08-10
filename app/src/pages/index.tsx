cb7d58ef4989e6e0a270e5733a2d60ce31d237e8
khai96_ <hvksmr1996@gmail.com>
2020-08-10 15:37:13 +0700

    Create a hello world page

---
+++ app/src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import HelloWorld from 'hello-world'
+import message from '@scope-name/scoped-hello-world'
+const Home = () => <HelloWorld message={message} />
+export default Home
