6703df5c607a9613ac8763e4e94111db3cb11e60
khai96_ <hvksmr1996@gmail.com>
2020-08-29 17:16:26 +0700

    Complete 'exclude'

--- app/gatsby-node.js
+++ app/gatsby-node.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const places = require('@tools/places')
+const pkgJson = require('./package.json')
 
 const packageNames = fs
   .readdirSync(places.packages)
@@ -11,7 +12,7 @@ const include = [
   __dirname,
   ...packageNames.map(name => path.join(__dirname, 'node_modules', name)),
 ]
-const exclude = fs.readdirSync(path.join(__dirname, 'node_modules')) // <- TODO
+const exclude = Object.keys(pkgJson.dependencies)
   .filter(name => !packageNames.includes(name))
   .map(name => path.join(__dirname, 'node_modules', name))
 
