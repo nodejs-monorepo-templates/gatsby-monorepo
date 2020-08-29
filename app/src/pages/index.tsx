4b20cdac94ef0cd416210944d5b8fe48d9b32721
khai96_ <hvksmr1996@gmail.com>
2020-08-29 21:51:48 +0700

    Define title

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import HelloWorld from 'hello-world'
-import { message } from '@scope-name/scoped-hello-world'
+import { title, message } from '@scope-name/scoped-hello-world'
 
 const Home = () => <>
   <Helmet>
     <meta charSet='UTF-8' />
-    <title>Hello, World!!</title>
+    <title>{title}</title>
   </Helmet>
   <HelloWorld message={message} />
 </>
--- packages/scoped-hello-world/index.ts
+++ packages/scoped-hello-world/index.ts
@@ -1,1 +1,2 @@
+export const title = '❤ Hello, World!! ❤'
 export const message = 'Hello, World!!'
