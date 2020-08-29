7bf8b7862add50bce209c7639c8712384bab527b
khai96_ <hvksmr1996@gmail.com>
2020-08-29 18:33:35 +0700

    Reorganize

--- app/gatsby-node.js
+++ app/gatsby-node.js
@@ -3,18 +3,18 @@ const fs = require('fs')
 const places = require('@tools/places')
 const pkgJson = require('./package.json')
 
-const packageNames = fs
-  .readdirSync(places.packages)
-  .map(name => path.join(places.packages, name, 'package.json'))
-  .filter(filename => fs.existsSync(filename))
-  .map(filename => require(filename).name)
-const watchIgnored = Object.keys(pkgJson.dependencies)
-  .filter(name => !packageNames.includes(name))
-  .map(name => path.join('node_modules', name))
-
 exports.onCreateWebpackConfig = ({
   actions,
 }) => {
+  const packageNames = fs
+    .readdirSync(places.packages)
+    .map(name => path.join(places.packages, name, 'package.json'))
+    .filter(filename => fs.existsSync(filename))
+    .map(filename => require(filename).name)
+  const watchIgnored = Object.keys(pkgJson.dependencies)
+    .filter(name => !packageNames.includes(name))
+    .map(name => path.join('node_modules', name))
+
   actions.setWebpackConfig({
     watchOptions: {
       ignored: watchIgnored,
