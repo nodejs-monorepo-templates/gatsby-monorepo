5cce7dc372ad3814f8ca6c68552ef7b9701c88e7
khai96_ <hvksmr1996@gmail.com>
2020-08-29 19:35:25 +0700

    Updates

---
+++ component---src-pages-index-tsx-8788a25091471c216ea1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{QeBL:function(e,t,n){"use strict";n.r(t);var i=n("tUyD"),a=n.n(i),l={display:"block",width:"100%",height:"100%",padding:"0px",margin:"0px"},s={textAlign:"center",verticalAlign:"middle",fontFamily:"sans-serif",display:"block",width:"100%",height:"100%"};function r(e){return a.a.createElement("main",{style:l},a.a.createElement("h1",{style:s},e.message))}t.default=function(){return a.a.createElement(r,{message:"Hello, World!!"})}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-8788a25091471c216ea1.js.map
