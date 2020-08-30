bea72424a90bb73fe76e5c1db1cedc67015b87aa
khai96_ <hvksmr1996@gmail.com>
2020-08-30 11:23:47 +0700

    Update test

--- test/scoped-hello-world/index.test.ts
+++ test/scoped-hello-world/index.test.ts
@@ -1,4 +1,8 @@
-import message from '@scope-name/scoped-hello-world'
+import { title, message } from '@scope-name/scoped-hello-world'
+
+it('title matches snapshot', () => {
+  expect(title).toMatchSnapshot()
+})
 
 it('message matches snapshot', () => {
   expect(message).toMatchSnapshot()
